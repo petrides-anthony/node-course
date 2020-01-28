3acd10d6227aa9506be62a7289f83bb1ca8999d0
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-01-28 08:59:54 +0000

    Finished Section: 4 Lecture: 16

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,44 @@
+const yargs = require('yargs')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
+// Customise yargs version
+yargs.version('1.1.0')
 
-console.log(msg)
+// Create add command
+yargs.command({
+    command: 'add', 
+    describe: "Add a new note",
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: "Removes a note",
+    handler: function () {
+        console.log('Removing the note!')
+    }
+})
+
+// List command
+yargs.command({
+    command: 'list',
+    describe: "Lists your notes",
+    handler: function () {
+        console.log('Listing out all notes!')
+    }
+})
+
+// Read command
+yargs.command({
+    command: 'read',
+    describe: "Read a note",
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)
